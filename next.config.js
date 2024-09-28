a11b3c3a89bce6215327c5e50bb05b62be9afe0a
techotaku1 <jsdg1818@gmail.com>
2024-09-27 23:22:22 -0500

    cambios

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  images: {
+    loader: 'custom',
+    loaderFile: './my-loader.js', // Cambia a .js si hiciste el cambio
+  },
 }
- 
-module.exports = nextConfig
+
+module.exports = nextConfig;
