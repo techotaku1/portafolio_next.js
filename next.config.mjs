949642ed7d3cca944c15ce7bf03a9afd6f1b2322
techotaku1 <jsdg1818@gmail.com>
2024-09-28 00:06:09 -0500

    Configuración para la exportación estática y manejo de imágenes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Habilita la exportación estática
+  reactStrictMode: true,
   images: {
     unoptimized: true, // Desactiva la optimización de imágenes
   },
