c72018d9f9af247f6dee2ded509efe5f1dc49165
techotaku1 <jsdg1818@gmail.com>
2024-11-25 18:46:09 -0500

    sdsd

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://josedavid-portafolio.vercel.app',
-  generateRobotsTxt: true, // Opcional: también genera robots.txt
-};
